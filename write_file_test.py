66c5a3f5f19a684490a9e2efda6728036facbc0b
Liam Heisler <Liam Heisler>
2016-05-11 00:02:42 -0400

    Added read file code

---
+++ write_file_test.py
@@ -0,0 +1,8 @@
+#use double quotes
+#use double slashes
+
+#insert file you wish to read here:
+
+file = "C:\\Users\\liamh\\Desktop\\Misc\\Random\\linuxIsCool.txt"
+text = open(file, 'r')
+print(text.read())
