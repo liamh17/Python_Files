29e85184956b025a5b06454a1579a5314119510e
Liam Heisler <Liam Heisler>
2016-05-23 19:35:40 -0400

    Updated gui

--- command_gui.py
+++ command_gui.py
@@ -1,11 +1,18 @@
+'''
+     Creator: Liam Heisler
+
+     This is a simple GUI that uses the "os.system"
+     command from python, allowing you to execute
+     terminal commands through the script.
+
+     There are specific buttons that call specific
+     commands, and they are neatly laid out in a simple
+     tkinter pythin GUI.
+
+'''
+
 from tkinter import *
-import random
 import os
-import warnings
-import winsound
-import time
-import sys
-from subprocess import *
 
 
 class Application(Frame):
@@ -20,65 +27,73 @@ class Application(Frame):
         self.instruction.grid(row=0, column=2,
         columnspan=2, sticky=W)
 
+###############################################################################
+
         self.exit_button = Button(self, text="exit",
-            command=self.exit_command, height=2, width=10)
+            command=self.exit_command, height=2, width=12)
         self.exit_button.grid(row=1, column=0, sticky=W)
 
         self.ipconfig_button = Button(self, text="ipconfig",
-            command=self.ipconfig, height=2, width=10)
+            command=self.ipconfig, height=2, width=12)
         self.ipconfig_button.grid(row=2, column=0, sticky=W)
 
         self.powershell_button = Button(self, text="powershell",
-            command=self.powershell, height=2, width=10)
+            command=self.powershell, height=2, width=12)
         self.powershell_button.grid(row=3, column=0, sticky=W)
 
         self.logoff_button = Button(self, text="logoff",
-            command=self.logoff, height=2, width=10)
+            command=self.logoff, height=2, width=12)
         self.logoff_button.grid(row=4, column=0, sticky=W)
 
         self.shutdown_button = Button(self, text="shutdown",
-            command=self.shutdown, height=2, width=10)
+            command=self.shutdown, height=2, width=12)
         self.shutdown_button.grid(row=5, column=0, sticky=W)
 
         self.shutdown_abort_button = Button(self, text="abort s.d.",
-            command=self.shutdown_abort, height=2, width=10)
+            command=self.shutdown_abort, height=2, width=12)
         self.shutdown_abort_button.grid(row=1, column=1, sticky=W)
 
         self.restart_button = Button(self, text="restart",
-            command=self.restart, height=2, width=10)
+            command=self.restart, height=2, width=12)
         self.restart_button.grid(row=2, column=1, sticky=W)
 
         self.chrome_button = Button(self, text="chrome",
-            command=self.chrome, height=2, width=10)
+            command=self.chrome, height=2, width=12)
         self.chrome_button.grid(row=3, column=1, sticky=W)
 
         self.netflix_button = Button(self, text="Netflix",
-            command=self.netflix, height=2, width=10)
+            command=self.netflix, height=2, width=12)
         self.netflix_button.grid(row=4, column=1, sticky=W)
 
         self.cbs_button = Button(self, text="CBS",
-            command=self.CBS, height=2, width=10)
+            command=self.CBS, height=2, width=12)
         self.cbs_button.grid(row=5, column=1, sticky=W)
 
         self.firewall_enable_button = Button(self, text="firewall ON",
-            command=self.firewall_enable, height=2, width=10)
+            command=self.firewall_enable, height=2, width=12)
         self.firewall_enable_button.grid(row=1, column=2, sticky=W)
 
         self.firewall_disable_button = Button(self, text="firewall OFF",
-            command=self.firewall_disable, height=2, width=10)
+            command=self.firewall_disable, height=2, width=12)
         self.firewall_disable_button.grid(row=2, column=2, sticky=W)
 
         self.flushdns_button = Button(self, text="flush DNS",
-            command=self.flushdns, height=2, width=10)
+            command=self.flushdns, height=2, width=12)
         self.flushdns_button.grid(row=3, column=2, sticky=W)
 
         self.test_connection_button = Button(self, text="ping test",
-            command=self.test_connection, height=2, width=10)
+            command=self.test_connection, height=2, width=12)
         self.test_connection_button.grid(row=4, column=2, sticky=W)
 
         self.regedit_button = Button(self, text="regedit",
-            command=self.firewall_disable, height=2, width=10)
+            command=self.firewall_disable, height=2, width=12)
         self.regedit_button.grid(row=5, column=2, sticky=W)
+
+        self.incognito_chrome_button = Button(self, text="incog. chrome",
+            command=self.incognito_chrome, height=2, width=12)
+        self.incognito_chrome_button.grid(row=1, column=3, sticky=W)
+
+###############################################################################
 
     def exit_command(self):
         #exits application
@@ -140,9 +155,15 @@ class Application(Frame):
         #opens registry editor
         os.system("regedit")
 
+    def incognito_chrome(self):
+        #starts incognito window
+        os.system("start chrome -incognito")
+
+###############################################################################
+
 root = Tk()
-root.title("Command GUI")
-root.geometry("450x240")
+root.title("Administrative Center")
+root.geometry("469x240")
 
 app = Application(root)
 root.mainloop()
