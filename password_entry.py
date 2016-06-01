6e11bf9b3cc1af558fd9ad7616486aca11b54103
Liam Heisler <Liam Heisler>
2016-06-01 17:19:23 -0400

    Updated files.

--- password_entry.py
+++ password_entry.py
@@ -1,23 +1,30 @@
 from tkinter import *
 import random
 import os
-import warnings
 import winsound
 import time
 import sys
-from subprocess import *
+import smtplib
+
 
 class Application(Frame):
-    """ GUI Application with buttons. """
-    global password
-    password = random.randint(1, 10)
-    password = abs(password)
+    """ GUI y with buttons. """
+    global password_entry
+    password_entry = random.randint(1, 10)
+    password_entry = abs(password_entry)
 
-    password_hint = password + 10
+    password_hint = password_entry + 10
     password_hint = str(password_hint)
 
-    print('Password hint, ' + password_hint + ' - 5(2)') 
-    
+    global server, fromaddr, toaddr, user, password
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    fromaddr = '17lheisler@fatherjudgestudent.com'
+    toaddr = '17lheisler@fatherjudgestudent.com'
+    user = '17lheisler@fatherjudgestudent.com'
+    password = 'yohihey123'
+
+    print('Password hint, ' + password_hint + ' - 5(2)')
+
     def __init__(self, master):
         """ Init the Frame """
         Frame.__init__(self, master)
@@ -26,56 +33,66 @@ class Application(Frame):
 
     def create_widgets(self):
         """ Create button, text, and entry """
-        self.instruction = Label(self, text = "Enter the password")
-        self.instruction.grid(row = 0, column = 0, columnspan = 2, sticky = W)
+        self.instruction = Label(self, text="Enter the password")
+        self.instruction.grid(row=0, column=0, columnspan=2, sticky=W)
 
-        self.password = Entry(self)
-        self.password.grid(row = 1, column = 1, sticky = W)
+        self.password_entry = Entry(self)
+        self.password_entry.grid(row=1, column=1, sticky=W)
 
-        self.submit_button = Button(self, text = "Submit", command = self.reveal)
-        self.submit_button.grid(row = 2, column = 0, sticky = W)
+        self.submit_button = Button(self, text="Submit",
+            command=self.reveal, width=5)
+        self.submit_button.grid(row=2, column=0, sticky=W)
+
+        self.exit_button = Button(self, text="Exit",
+            command=self.quit, width=5)
+        self.exit_button.grid(row=2, column=1, sticky=W)
 
         #self.close_button = Button(self, text = "Close", command = self.quit)
         #self.close_button.grid(row = 2, column = 0, sticky = E)
 
-        self.text = Text(self, width = 35, height = 5, wrap = WORD)
-        self.text.grid(row = 3, column = 0, columnspan = 2, sticky = W)
+        self.text = Text(self, width=35, height=5, wrap=WORD)
+        self.text.grid(row=3, column=0, columnspan=2, sticky=W)
 
     def quit(self):
-        self.destroy() 
-        
+        sys.exit()
+
     def reveal(self):
+
         """ Display message based on password typed in """
-        content = self.password.get()
+        content = self.password_entry.get()
         content = int(content)
 
-        if content == password:
+        if content == password_entry:
+            #do something more with this
             message = "Access granted."
 
-            os.system('start chrome liamh17.github.io')
-            
+            # email text
+            granted_msg = 'Access granted via login system.'
+
+            server.starttls()
+            server.login(user, password)
+            server.sendmail(fromaddr, toaddr, granted_msg)
+
+            # will auto log onto gmail acc
+            os.system("start chrome www.gmail.com")
         else:
+            # email text
+            denied_msg = 'Someone failed to login to your secure system!'
+
+            # print you were denied access to actual text handler
             message = "Access denied."
 
-            os.system("start chrome liamh17.github.io")
-            
-            winsound.Beep(600, 800)
-            time.sleep(.2)
-            winsound.Beep(600, 800)
-            time.sleep(.2)
-            winsound.Beep(600, 800)
+            server.starttls()
+            server.login(user, password)
+            server.sendmail(fromaddr, toaddr, denied_msg)
 
-            #espeak.synth('Acces denied, try again')
-            #os.system('shutdown -r -t 30 -c "Wrong password. Goodbye.."')
-            
-        #row, column (first parameter)
-        self.text.delete(0.0, END) 
+        self.text.delete(0.0, END)
         self.text.insert(0.0, message)
-        
+
+
 root = Tk()
 root.title("Password entry")
 root.geometry("250x150")
 app = Application(root)
 
 root.mainloop()
-        
